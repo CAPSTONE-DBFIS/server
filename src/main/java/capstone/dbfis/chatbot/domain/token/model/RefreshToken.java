9047d5303cc59f77d06dd199ecd3ad198a18d7b5
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2024-11-03 18:21:46 +0900

    feature/security: Add Refresh Token model, repository

---
+++ src/main/java/capstone/dbfis/chatbot/domain/token/model/RefreshToken.java
@@ -0,0 +1,34 @@
+package capstone.dbfis.chatbot.domain.token.model;
+
+import capstone.dbfis.chatbot.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    // Member 엔티티와의 관계 설정
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "member_id", nullable = false, referencedColumnName = "id", unique = true)
+    private Member member;
+
+    @Column(name = "refresh_token", nullable = false)
+    private String refreshToken;
+
+    public RefreshToken(Member member, String refreshToken) {
+        this.member = member;
+        this.refreshToken = refreshToken;
+    }
+
+    public RefreshToken update(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
+        return this;
+    }
+}
