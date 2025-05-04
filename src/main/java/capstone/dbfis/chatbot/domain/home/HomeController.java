1a2cf3f7fbc6130ab09efc0d1bcda38dbac6fd7d
TUAZAR <100247125+TUAZAR@users.noreply.github.com>
2025-05-04 19:52:16 +0900

    feature: frontend 임시 렌더링 사이트

--- src/main/java/capstone/dbfis/chatbot/domain/home/HomeController.java
+++ src/main/java/capstone/dbfis/chatbot/domain/home/HomeController.java
@@ -35,4 +35,9 @@ public class HomeController {
     public String insightPage() {
         return "insight";
     }
+
+    @GetMapping("/foreign-insight")
+    public String foreignInsightPage() {
+        return "foreign-insight";
+    }
 }
